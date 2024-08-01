1f66864744b1f3df6d9abc795f8d46b2bbaaa0b8
bom04 <gil1122@naver.com>
2024-08-01 23:39:04 +0900

    feat: s3 preSigned url로 파일 업로드 구현

---
+++ src/main/java/subscribers/clearbunyang/s3/FileController.java
@@ -0,0 +1,18 @@
+package subscribers.clearbunyang.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FileController {
+    private final FileService fileService;
+
+    @PostMapping("/preSigned-url")
+    public String getPreSignedUrl(@RequestParam String directoryName, @RequestParam String fileName) {
+        return fileService.getPreSignedUrl(directoryName,fileName);
+    }
+
+}
