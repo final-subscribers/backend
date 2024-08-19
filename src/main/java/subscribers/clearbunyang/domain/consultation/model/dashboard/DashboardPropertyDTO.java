a3b096f6396f02ca32bdd4c845d0ada9c767dba0
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-08-19 18:29:58 +0900

    chore: 잘못된 argument 및 네이밍 수정

--- src/main/java/subscribers/clearbunyang/domain/consultation/model/dashboard/DashboardPropertyDTO.java
+++ src/main/java/subscribers/clearbunyang/domain/consultation/model/dashboard/DashboardPropertyDTO.java
@@ -1,6 +1,8 @@
 package subscribers.clearbunyang.domain.consultation.model.dashboard;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,14 +17,15 @@ import subscribers.clearbunyang.domain.consultation.entity.enums.Status;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DashboardPropertyDTO {
     String propertyName;
     Status status;
     Period period;
-    Integer all;
     Integer pending;
     Integer completed;
-    String phone;
+    Integer all;
+    Integer phone;
     Integer channel;
     Integer lms;
     List<ConsultationProgressDTO> eachPeriod;
