a06f9d4fcef1466e013df7f86b8d89417db109dc
jisun.park <pjs981114@naver.com>
2024-08-20 16:45:29 +0900

    feat: 접근 권한 없는 페이지로 접근 시 unauthorized 반환하도록 추가

--- src/main/java/subscribers/clearbunyang/global/config/SecurityConfig.java
+++ src/main/java/subscribers/clearbunyang/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package subscribers.clearbunyang.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import subscribers.clearbunyang.global.security.filter.AuthenticationFilter;
 import subscribers.clearbunyang.global.token.JwtTokenProcessor;
@@ -54,7 +56,11 @@ public class SecurityConfig {
                                         .permitAll())
                 .addFilterBefore(
                         new AuthenticationFilter(jwtTokenProcessor),
-                        UsernamePasswordAuthenticationFilter.class);
+                        UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(
+                        exception ->
+                                exception.authenticationEntryPoint(
+                                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
 
         return http.build();
     }
