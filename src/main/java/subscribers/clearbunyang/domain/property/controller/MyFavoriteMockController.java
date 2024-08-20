bec9b57f6c1161e3dbc6fb68a4b2007a7c7b585d
jisun.park <pjs981114@naver.com>
2024-08-20 14:58:04 +0900

    feat: 나의 상담 목록, 찜 목록 pagination까지 완료

--- src/main/java/subscribers/clearbunyang/domain/property/controller/MyFavoriteMockController.java
+++ src/main/java/subscribers/clearbunyang/domain/property/controller/MyFavoriteMockController.java
@@ -1,13 +1,16 @@
 package subscribers.clearbunyang.domain.property.controller;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyFavoriteMockController {
 
     @GetMapping
-    public Map<String, Object> getMyFavorites() {
+    public Map<String, Object> getMyFavorites(
+            @RequestParam(name = "status") String status,
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "size", defaultValue = "12") int size) {
+
+        int offset = (page - 1) * size;
+        List<Map<String, Object>> properties = new ArrayList<>();
 
         List<List<String>> keywords =
                 Arrays.asList(
@@ -24,205 +33,29 @@ public class MyFavoriteMockController {
                         Arrays.asList("지하철", "편의시설"),
                         Arrays.asList("버스", "편의시설"));
 
-        // 매물 정보
-        List<Map<String, Object>> properties =
-                Arrays.asList(
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        },
-                        new HashMap<String, Object>() {
-                            {
-                                put("keyword", keywords);
-                                put(
-                                        "image_url",
-                                        "https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA0MjhfMjYz%2FMDAxNTI0OTAyMDI3ODAz.MFDCwAmFB91efs0LS0Bm07NjmuUOJDkEh-h3nvC9kJ0g.s9XRo7n9wR1Pp4hZUP0avxIl-Ct9W0FCBKU07-iC-nAg.PNG.skywnb%2FexternalFile.png&type=sc960_832");
-                                put("name", "잠실 프루지오");
-                                put("area_addr", "서울특별시 서초구 ");
-                                put("created_at", "2024-07-20");
-                                put("property_type", "아파트");
-                                put("sales_type", "민간 분양");
-                                put("count", 100);
-                                put("price", 29000);
-                                put("sales_price", 27000);
-                            }
-                        });
+        for (int i = 0; i < 20; i++) {
+            HashMap<String, Object> property = new HashMap<>();
+            property.put("keyword", keywords);
+            property.put("name", "잠실 프루지오" + (i + 1));
+            property.put("area_addr", "서울특별시 서초구 ");
+            property.put("created_at", "2024-07-20");
+            property.put("property_type", "아파트");
+            property.put("sales_type", "민간 분양");
+            property.put("count", 100);
+            property.put("price", 29000);
+            property.put("sales_price", 27000);
+            properties.add(property);
+        }
+
+        List<Map<String, Object>> pagedData =
+                properties.stream().skip(offset).limit(size).collect(Collectors.toList());
 
         Map<String, Object> response = new HashMap<>();
-        response.put("favorite_number", 21);
-        response.put("properties", properties);
+        response.put("favorite_number", 20);
+        response.put("totalPages", (int) Math.ceil((double) properties.size() / size));
+        response.put("pageSize", size);
+        response.put("currentPage", page);
+        response.put("properties", pagedData);
 
         return response;
     }
