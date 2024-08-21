b42e707ccb215fa93025a04e99e3400313dac16a
fullm <fullmoon0207@naver.com>
2024-08-21 23:17:54 +0900

    feat: MethodArgumentTypeMismatchException 핸들러 등록

--- src/main/java/subscribers/clearbunyang/global/exception/GlobalExceptionHandler.java
+++ src/main/java/subscribers/clearbunyang/global/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ package subscribers.clearbunyang.global.exception;
 import jakarta.annotation.Priority;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import subscribers.clearbunyang.global.api.Response;
 import subscribers.clearbunyang.global.api.Result;
 import subscribers.clearbunyang.global.exception.Invalid.InvalidValueException;
+import subscribers.clearbunyang.global.exception.errorCode.ErrorCode;
 import subscribers.clearbunyang.global.exception.notFound.EntityNotFoundException;
 
 @Priority(Integer.MAX_VALUE)
@@ -25,5 +27,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public Response<Void> handleCustomException(CustomException e) {
         return Response.<Void>builder().result(Result.Error(e.getErrorCode())).build();
+    }
+
+    // getCompletedResponse 서 잘못된 양식의 tier를 입력했을 때
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public Response<Void> methodArgumentTypeException(MethodArgumentTypeMismatchException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        return Response.<Void>builder().result(Result.Error(errorCode)).build();
     }
 }
