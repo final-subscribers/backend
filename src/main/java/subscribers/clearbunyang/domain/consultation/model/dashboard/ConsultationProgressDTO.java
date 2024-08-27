e465a27033b4e9ec35eea4d754d24b0aafc68172
fullm <fullmoon0207@naver.com>
2024-08-27 12:45:16 +0900

    feat: pull origin dev

---
+++ src/main/java/subscribers/clearbunyang/domain/consultation/model/dashboard/ConsultationProgressDTO.java
@@ -0,0 +1,28 @@
+package subscribers.clearbunyang.domain.consultation.model.dashboard;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ConsultationProgressDTO {
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+    String periodLabel;
+    String propertyName;
+    Integer pending;
+    Integer completed;
+    Integer all;
+}
