003d5570bd991d0b4ec82cc9dd828879c3403e29
이유정 <epoint8989@gmail.com>
2024-08-28 18:14:07 +0900

    feat: 좋아요 목록 반환

--- src/main/java/subscribers/clearbunyang/domain/property/repository/PropertyRepository.java
+++ src/main/java/subscribers/clearbunyang/domain/property/repository/PropertyRepository.java
@@ -1,7 +1,24 @@
 package subscribers.clearbunyang.domain.property.repository;
 
 
+import java.time.LocalDate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import subscribers.clearbunyang.domain.property.entity.Property;
+import subscribers.clearbunyang.domain.user.entity.Member;
 
-public interface PropertyRepository extends JpaRepository<Property, Long> {}
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+    @Query(
+            "SELECT p FROM Property p JOIN p.likes l WHERE l.member = :member AND "
+                    + "(:isOpen = true AND p.startDate <= :currentDate AND p.endDate >= :currentDate) "
+                    + "OR (:isOpen = false AND p.endDate < :currentDate)")
+    Page<Property> findAllByMemberAndDateRange(
+            @Param("member") Member member,
+            @Param("currentDate") LocalDate currentDate,
+            @Param("pageable") Pageable pageable,
+            @Param("isOpen") boolean isOpen);
+}
