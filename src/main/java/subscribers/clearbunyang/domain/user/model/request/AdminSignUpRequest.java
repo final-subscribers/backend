cab4cc4eacc959924320606f45560df15e0542e1
이유정 <epoint8989@gmail.com>
2024-08-28 15:49:36 +0900

    refactor: request 카멜케ã이스로 변경

--- src/main/java/subscribers/clearbunyang/domain/user/model/request/AdminSignUpRequest.java
+++ src/main/java/subscribers/clearbunyang/domain/user/model/request/AdminSignUpRequest.java
@@ -1,7 +1,6 @@
 package subscribers.clearbunyang.domain.user.model.request;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,23 +20,18 @@ public class AdminSignUpRequest {
 
     @PasswordValidation private String password;
 
-    @JsonProperty("phone_number")
     private String phoneNumber;
 
-    @JsonProperty("company_name")
     private String companyName;
 
-    @JsonProperty("registration_number")
     private Long registrationNumber;
 
     private String address;
 
     private String business;
 
-    @JsonProperty("housing_file")
     private FileInfo housingFile;
 
-    @JsonProperty("registration_file")
     private FileInfo registrationFile;
 
     @Getter
