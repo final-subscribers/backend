fdb36ac3f34ea80961e4d9393f5d230533c9b344
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-09-06 09:26:49 +0900

    feat: 메서드 단위 Cacheing 설정 추가

--- src/main/java/subscribers/clearbunyang/global/config/RedisConfig.java
+++ src/main/java/subscribers/clearbunyang/global/config/RedisConfig.java
@@ -1,13 +1,19 @@
 package subscribers.clearbunyang.global.config;
 
 
+import java.time.Duration;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -16,6 +22,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${redis.host}") private String host;
@@ -50,5 +57,29 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    // 캐시 매니저 설정
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(10)) // 캐시 수명 설정
+                        .disableCachingNullValues();
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfig).build();
+    }
+
+    // 메소드 단위로 캐시를 위한 기본 키 생성기
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getSimpleName());
+            sb.append(".").append(method.getName());
+            for (Object param : params) {
+                sb.append(".").append(param.toString());
+            }
+            return sb.toString();
+        };
     }
 }
