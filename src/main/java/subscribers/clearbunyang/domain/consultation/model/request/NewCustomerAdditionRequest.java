f72e1ccdf6cabca54e7680ae217fa9d1c7688f8f
fullm <fullmoon0207@naver.com>
2024-09-15 00:02:30 +0900

    feat: 검증 어노테이션 추가

--- src/main/java/subscribers/clearbunyang/domain/consultation/model/request/NewCustomerAdditionRequest.java
+++ src/main/java/subscribers/clearbunyang/domain/consultation/model/request/NewCustomerAdditionRequest.java
@@ -28,8 +28,7 @@ public class NewCustomerAdditionRequest {
     @Size(min = 11, max = 11)
     private String phoneNumber; // 고객 폰 번호
 
-    // @NotBlank 테스트를 위해 주석
-    private String consultant; // 상담사
+    @NotBlank private String consultant; // 상담사
 
     @NotNull @FutureOrPresent private LocalDate preferredAt;
 
