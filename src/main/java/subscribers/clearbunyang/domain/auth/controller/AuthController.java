e4b05934acc4d09e692fc8e40cd519d941adb7ef
bom04 <gmaru6420@gmail.com>
2024-09-22 15:30:10 +0900

    refactor: 로그아웃을 post로 변경

--- src/main/java/subscribers/clearbunyang/domain/auth/controller/AuthController.java
+++ src/main/java/subscribers/clearbunyang/domain/auth/controller/AuthController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,7 +52,7 @@ public class AuthController {
     }
 
     @Operation(summary = "로그아웃")
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public void logout(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         String redirectUri = authService.logout(request, response);
